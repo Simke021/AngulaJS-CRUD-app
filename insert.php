2d213864fccbb23da3ca3b9bc61bc6e17a73b64a
Aleksandar Ljubišić <simke20@yahoo.com>
2017-09-18 18:40:04 +0200

    Insert data into db

---
+++ insert.php
@@ -0,0 +1,21 @@
+<?php
+// konekcija sa bazom
+$conn = new mysqli("localhost", "root", "", "angular_crud");
+//
+$data = json_decode(file_get_contents("php://input"));
+
+if(count($data) > 0){
+    // malo validacije i unos u bazu
+    $first_name = mysqli_real_escape_string($conn, $data->firstname);
+    $last_name  = mysqli_real_escape_string($conn, $data->lastname);
+
+    $query = "INSERT INTO users(f_name, l_name) VALUES ('$first_name', '$last_name')";
+
+    if(mysqli_query($conn, $query)){
+        echo "Data Inserted Successfully.";
+    }else{
+        echo "Error. Pleasse try latter.";
+    }
+}
+
+?>
